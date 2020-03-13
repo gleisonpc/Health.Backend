6e7e120790311d40408d8c769a4a7c68dbc62913
Gleison Silva <gleison.silva@programmers.com.br>
2020-03-13 15:56:32 -0300

    Remover Value controller e outros ajustes

--- Health.Backend/Health.Backend.Domain.Tests/Services/PrecoServiceTest.cs
+++ Health.Backend/Health.Backend.Domain.Tests/Services/PrecoServiceTest.cs
@@ -1,5 +1,4 @@
 ﻿using Health.Backend.Domain.Repositories.Interfaces;
-using Health.Backend.Domain.Services;
 using Health.Backend.Domain.Services.Interfaces;
 using Moq;
 
@@ -16,17 +15,6 @@ namespace Health.Backend.Domain.Tests.Services
         public PrecoServiceTest()
         {
             _coberturaRepository = new Mock<ICoberturaRepository>(BEHAVIOR);
-            _precoService = new PrecoService(_coberturaRepository.Object);
         }
-
-        //[Fact]
-        //public void Calcular_Preco_Para_Segurado()
-        //{
-        //    var mockCobertura = new CoberturasMock();
-        //    var mockSegurado = new SeguradoMock(mockCobertura.Coberturas);
-        //    _coberturaRepository.Setup(x => x.ObterCoberturas()).Returns(mockCobertura.Coberturas);
-
-        //    var preco = _precoService.ObterPrecoParaSegurado(mockSegurado.Segurado);
-        //}
     }
 }
